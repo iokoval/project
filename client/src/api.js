e0a2f4cd426bfcdfddf7a42f08a8d6dba2456c3e
Ilya Koval <ilya.koval.2018@gmail.com>
2021-10-18 00:54:55 +0300

    ip for src/api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "http://localhost:4000/api",
+    baseURL: "http://3.132.108.201:4000/api",
 })
 // products
 export const insertProduct = payload => api.post('/product', payload);
